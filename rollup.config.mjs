b695b416f6605754573b18403673b738076ef4b4
Abdulsalam Odetayo <adeodetayo92@gmail.com>
2023-01-27 11:17:00 -0500

    Prepare library for publish

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import packageJson from "./package.json" assert { type: "json" };
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import terser from "@rollup/plugin-terser";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      terser(),
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        exclude: ["**/__tests__", "**/*.test.ts"],
+      }),
+    ],
+    external: ["react-dom"],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-import colorMap, { createColor } from "./color-map";
-import { test_colors, test_ranges } from "./tests/data";
-
-const temperatureMap = colorMap(test_colors, test_ranges);
-const temp1 = temperatureMap.getColor(30);
-console.log(temperatureMap.map.ranges);
-console.log(temp1?.rgb); // returns { r: 255, g: 22, b: 0 }
-console.log(temp1?.toString()); // returns "rgb(255, 22, 0)"
-console.log(temp1?.toHex()); // returns "#ff1600"
-
-const color = createColor([3, 40, 69]);
-console.log(color.toString()); // returns "rgb(3, 40, 69)"
-console.log(color.toHex()); // returns "#032845"
+export { default as colorMap, createColor } from "./color-map";
+export * from './utils'
